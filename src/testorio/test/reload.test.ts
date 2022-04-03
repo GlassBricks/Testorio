4055f44158486e33924fcd977f684338864d42e4
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2022-04-03 12:35:35 -0700

    Refactor reload test

--- src/testorio/test/reload.test.ts
+++ src/testorio/test/reload.test.ts
@@ -12,18 +12,25 @@ let someValue = "initial"
 
 const refValue = {}
 
+// this function holds a reference to "refValue" as an upvalue
+// this reference should be dropped during reload, i.e. this function should not be saved in global
+// otherwise this may cause problems when using weak tables in global: the value is saved as it is still referenced
+// by the function. However, during load, the value is no longer referenced, so when making the table weak during
+// on_load the global table will change, causing an error.
+
+// multiple tests in one, as reload is slow
 test("after_mod_reload", () => {
-  global.foo = () => 0 // can't be serialized
   someValue = "changed"
-  // this function holds a reference to "refValue" as an upvalue
-  // this reference should be dropped during reload, i.e. this function should not be saved in global
-  // otherwise this may cause problems if a table is made weak during on_load: the reference is no longer valid,
-  // and so the global table is changed, causing an error
+
+  global.foo = () => 0 // can't be serialized
+
   global.weakTable = setmetatable({ value: refValue }, { __mode: "v" })
 }).after_mod_reload(() => {
-  assert.is_nil(global.foo)
-  assert.equal("initial", someValue)
   assert.equal(getTestState().getTestStage(), TestStage.Running)
+
+  assert.equal("initial", someValue)
+
+  assert.is_nil(global.foo)
 
   assert.is_nil(global.weakTable.value)
 })
